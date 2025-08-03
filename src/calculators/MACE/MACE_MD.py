7580217deb14495d26ef7644bd58ecdd94edb66c
Jorge Fabila <jorgefabila1997@gmail.com>
2025-08-03 15:34:47 +0200

    nombre de archivo añadido

--- src/calculators/MACE/MACE_MD.py
+++ src/calculators/MACE/MACE_MD.py
@@ -13,10 +13,11 @@ from ase.md.verlet import VelocityVerlet
 from ase.md.langevin import Langevin
 from ase.md.nvtberendsen import NVTBerendsen
 
-def MACE_collision(atoms,config):
+def MACE_collision(atoms,config,index):
     #Stationary(atoms)
     #ZeroRotation(atoms)#atoms.set_calculator(mace_calc)
     mace_calc = MACECalculator(model_paths=[config["mace_force_field_file"]], device='cpu', default_dtype="float32")
+
     atoms.calc = mace_calc
     if config["thermostat"] == "Langevin":
         dyn = Langevin(atoms,timestep=1.0 * units.fs, temperature_K=config["temperature"],friction=0.1)
@@ -25,6 +26,12 @@ def MACE_collision(atoms,config):
     elif config["thermostat"] == "NVTBerendsen":
         dyn = NVTBerendsen(atoms, 2 * units.fs, temperature_K=config["temperature"], taut=0.5*100000*units.fs)
     timesteps = 1000 # config["timesteps"]
+
+    if config["collisions_files"] is not None:
+        output_file = config["collisions_files"]+"_"+str(index)+".xyz"
+    else:
+        output_file = sys.stdout
+
     for i in range(timesteps):
         dyn.step()
         dyn.atoms.write(output_file, append=True)
