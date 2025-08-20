34a0b5d1506265faf1f1f3c6441acbccd1ebef81
Jorge Fabila <jorgefabila1997@gmail.com>
2025-08-20 20:06:19 +0200

    Naggs :: RelaxGeometry :: primeraversión funcional

--- src/RelaxGeometry.py
+++ src/RelaxGeometry.py
@@ -13,7 +13,10 @@ from ase.md.verlet import VelocityVerlet
 from ase.md.langevin import Langevin
 from ase.md.nvtberendsen import NVTBerendsen
 
+from src.utils import CheckConnectivity, GetParticleDiameter
+
 def RelaxGeometry(atoms,config,index):
+    rho = GetParticleDiameter(atoms)
     if config["calculator"].upper() == "MACE":
         calc = MACECalculator(model_paths=[config["mace_force_field_file"]], device='cpu', default_dtype="float32")
     elif config["calculator"].upper() == "LAMMPS":
@@ -22,11 +25,11 @@ def RelaxGeometry(atoms,config,index):
 
     atoms.calc = calc
     timestep_ = config["time_step_relaxation"]
-    if config["thermostat"] == "Langevin":
+    if config["thermostat_relaxation"] == "Langevin":
         dyn = Langevin(atoms,timestep=timestep_ * units.fs, temperature_K=config["temperature"],friction=0.1)
-    elif config["thermostat"] == "Verlet":
+    elif config["thermostat_relaxation"] == "Verlet":
         dyn = VelocityVerlet(atoms,timestep=timestep_*units.fs)
-    elif config["thermostat"] == "NVTBerendsen":
+    elif config["thermostat_relaxation"] == "NVTBerendsen":
         dyn = NVTBerendsen(atoms, timestep_ * units.fs, temperature_K=config["temperature"], taut=0.5*100000*units.fs)
 
     if config["collisions_files"] is not None:
@@ -34,8 +37,16 @@ def RelaxGeometry(atoms,config,index):
     else:
         output_file = sys.stdout
 
-    for i in range(config["relaxation_steps"]):
+    i = 0
+    while i < config["relaxation_steps"]:
         dyn.step()
         dyn.atoms.write(output_file, append=True)
-
+        if i % 100 == 0:
+            max_dist = dyn.atoms.get_all_distances().max()
+            if max_dist > 3*rho: #float(config["dist_from_seed"]):
+                break
+#            connectivity = CheckConnectivity(dyn.atoms, 3.5)
+#            if connectivity == False:
+#                break
+        i = i + 1
     return atoms
