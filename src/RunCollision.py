fd1a0404ae6cc23e1c1305cb282b9b4428de6166
Jorge Fabila <jorgefabila1997@gmail.com>
2025-08-20 20:06:56 +0200

    Naggs :: RunCollision :: Primera versión funcional

--- src/RunCollision.py
+++ src/RunCollision.py
@@ -13,7 +13,10 @@ from ase.md.verlet import VelocityVerlet
 from ase.md.langevin import Langevin
 from ase.md.nvtberendsen import NVTBerendsen
 
+from src.utils import CheckConnectivity, GetParticleDiameter
+
 def RunCollision(atoms,config,index):
+    rho = GetParticleDiameter(atoms)
     if config["calculator"].upper() == "MACE":
         calc = MACECalculator(model_paths=[config["mace_force_field_file"]], device='cpu', default_dtype="float32")
     elif config["calculator"].upper() == "LAMMPS":
@@ -22,11 +25,11 @@ def RunCollision(atoms,config,index):
 
     atoms.calc = calc
     timestep_ = config["time_step_collision"]
-    if config["thermostat"] == "Langevin":
+    if config["thermostat_collision"] == "Langevin":
         dyn = Langevin(atoms,timestep=timestep_ * units.fs, temperature_K=config["temperature"],friction=0.1)
-    elif config["thermostat"] == "Verlet":
+    elif config["thermostat_collision"] == "Verlet":
         dyn = VelocityVerlet(atoms,timestep=timestep_ *units.fs)
-    elif config["thermostat"] == "NVTBerendsen":
+    elif config["thermostat_collision"] == "NVTBerendsen":
         dyn = NVTBerendsen(atoms,timestep= timestep_ * units.fs, temperature_K=config["temperature"], taut=0.5*100000*units.fs)
 
     if config["collisions_files"] is not None:
@@ -34,8 +37,14 @@ def RunCollision(atoms,config,index):
     else:
         output_file = sys.stdout
 
-    for i in range(config["collision_steps"]):
+    i = 0
+    while i < config["collision_steps"]:
         dyn.step()
         dyn.atoms.write(output_file, append=True)
+        if i % 100 == 0:
+            max_dist = dyn.atoms.get_all_distances().max()
+            if max_dist > 3*rho: #float(config["dist_from_seed"]):
+                break
+        i = i + 1
 
     return atoms
