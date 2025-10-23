e428e8dd65d2ca0cb4530a50a167363ce2fb5865
Jorge Fabila <jorgefabila1997@gmail.com>
2025-10-23 13:45:18 +0200

    funcionando

--- modos_normales.py
+++ modos_normales.py
@@ -30,24 +30,29 @@ def read_xyz_with_modes(filename, n_conf):
     modes = np.array([data_matrix[:, 3 + 3*i : 6 + 3*i] for i in range(nmodes)])
     return symbols, positions, modes, freq
 
-symbols, positions, modes, freq = read_xyz_with_modes("TiC_32_4x4x4_-4C.xyz",)
-generate_displaced_structures(symbols, positions, modes, delta=4)
 
-def generate_displaced_structures(symbols, positions, modes, delta=0.1, outdir='displacements'):
-    print("POSI", positions)
-    print("MODES", modes)
+def generate_displaced_structures(symbols, positions, modes, delta=0.1, file="file", outdir='displacements'):
+#    print("POSI", positions)
+#    print("MODES", modes)
     # 60,3 ; 1,60,3
     norms = np.linalg.norm(modes, axis=-1, keepdims=True)
     modes = modes / norms
     os.makedirs(outdir, exist_ok=True)
     displaced_positions = positions + modes[0,:,:] * delta
     atoms = Atoms(symbols=symbols, positions=displaced_positions)
-    filename = os.path.join(outdir, f"mode_.xyz")
+    filename = os.path.join(outdir, file)
     write(filename, atoms)
     print(f"Guardado: {filename}")
 
 
 if __name__ == "__main__":
-    xyz_file = "molecule_modes.xyz"
-    symbols, positions, modes, freq = read_xyz_with_modes(xyz_file)
-    generate_displaced_structures(symbols, positions, modes, delta=0.1)
+    xyz_file = "tic32.xyz"
+    n_points = 100
+    amplitude = 1.0
+    mode_n = 4
+    symbols, positions, modes, freq = read_xyz_with_modes(xyz_file,mode_n)
+    theta = np.linspace(0, 2 * np.pi, n_points)
+    y = amplitude * np.sin(theta)
+    for i in range(n_points):
+        file = "mode_"+str(mode_n)+"_"+str(i)+".xyz"
+        generate_displaced_structures(symbols, positions, modes, delta=y[i],file = file)
